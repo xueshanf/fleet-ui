55983b5a4376afa08451c7bf291ca74fa4d00ad2
CoreOS Admin <core@ip-10-42-2-36.us-west-2.compute.internal>
2015-03-31 00:10:21 +0000

    add status interval in run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 export ETCD_PEER=${ETCD_PEER:-$(/sbin/ip route|awk '/default/ { print $3 }')}
+export STATUS_INTERVAL=${STATUS_INTERVAL:-70000}
 
 if [ -f /root/id_rsa ]; then
   eval `ssh-agent -s` && ssh-add /root/id_rsa
