2bc917f5a9da4a8aa0ba3d83d9d6eca1a1086b90
Xueshan Feng <xueshan.feng@gmail.com>
2015-07-16 14:49:29 -0700

    mount path of /root/id_rsa into the container.
    Also fix mode.

--- run.sh
+++ run.sh
@@ -3,7 +3,8 @@
 export ETCD_PEER=${ETCD_PEER:-$(/sbin/ip route|awk '/default/ { print $3 }')}
 export STATUS_INTERVAL=${STATUS_INTERVAL:-70000}
 
-if [ -f /root/id_rsa ]; then
+if [ -f /root/id_rsa/id_rsa ]; then
+  chmod 700 /root/id_rsa/ && chmod 600 /root/id_rsa/id_rsa
   eval `ssh-agent -s` && ssh-add /root/id_rsa
 fi
 
