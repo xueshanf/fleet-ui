538bed00af8179ba19e11a34fca55f17c5fcb3cb
Xueshan Feng <xueshan.feng@gmail.com>
2015-08-09 11:32:00 -0700

    id_rsa is passed in as a file.

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ export ETCD_PEER=${ETCD_PEER:-$(/sbin/ip route|awk '/default/ { print $3 }')}
 export STATUS_INTERVAL=${STATUS_INTERVAL:-70000}
 
 if [ -f /root/id_rsa ]; then
-  chmod 700 /root/id_rsa/ && chmod 600 /root/id_rsa/id_rsa
+  chmod 600 /root/id_rsa
   eval `ssh-agent -s` && ssh-add /root/id_rsa
 fi
 
